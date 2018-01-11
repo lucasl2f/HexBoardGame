a6b8f351696a4f13c005b1a4fc6dc5a28b2587a0
Lucas Fonseca Desktop <lfonseca010@gmail.com>
2018-01-11 00:00:05 -0200

    Camera movement (WASD)

---
+++ Graph/Assets/_Scripts/CameraMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	[SerializeField]
+	float movement = 1;
+
+	bool changed;
+
+	Transform myTransform;
+	Vector3 posTemp;
+
+	void Awake() {
+		myTransform = gameObject.transform;
+	}
+
+	void LateUpdate () {
+		if (Input.GetKey(KeyCode.W)) {
+			posTemp.z += movement;
+			changed = true;
+		}
+		if (Input.GetKey(KeyCode.A)) {
+			posTemp.x -= movement;
+			changed = true;
+		}
+		if (Input.GetKey(KeyCode.S)) {
+			posTemp.z -= movement;
+			changed = true;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			posTemp.x += movement;
+			changed = true;
+		}
+
+		if (changed) {
+			myTransform.position += posTemp;
+			posTemp = Vector3.zero;
+			changed = false;
+		}
+	}
+}
